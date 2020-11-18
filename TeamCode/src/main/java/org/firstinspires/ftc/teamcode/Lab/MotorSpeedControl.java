41c3630dfc00555ea57afe152f3275f7f6a36054
john.duval <john.duval@mtbethelchristian.org>
2020-11-18 14:23:55 -0500

    Motor Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lab/MotorSpeedControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lab/MotorSpeedControl.java
@@ -16,7 +16,7 @@ public class MotorSpeedControl extends OpMode {
 
     public ElapsedTime TeleOpTime;
 
-    public double targetHighGoalRPMs = 5000;
+    public double targetHighGoalRPMs = 2500;
 
     public double encodersPerSecond = 0;
 
@@ -24,7 +24,10 @@ public class MotorSpeedControl extends OpMode {
     public boolean initTeleOpToggle = true;
     public LinearOpMode linearOp = null;
 
-    public double motorSpeed = 0.5;
+    public double motorSpeed = 0.75;
+
+    public double minMotorPower = 0.6;
+    public double maxMotorPower = 0.9;
 
     public boolean powerMode = true;
 
@@ -32,25 +35,22 @@ public class MotorSpeedControl extends OpMode {
 
     public double RPMs = 0;
 
+    public double encodersResetCount = 0;
+
     @Override
     public void init() {
-
-
         myMotor = hardwareMap.dcMotor.get ("launcher_motor");
         myMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         setMotorRunModes(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         setMotorRunModes(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         TeleOpTime = new ElapsedTime();
-
-
     }
 
     @Override
     public void loop() {
-        encodersPerSecond = (myMotor.getCurrentPosition() / TeleOpTime.seconds());
+        encodersPerSecond = ((myMotor.getCurrentPosition() - encodersResetCount) / TeleOpTime.seconds());
         RPMs = (encodersPerSecond / ENCODER_PPR) * 60;
-
 
         if (initTeleOpToggle == true) {
             initTeleOp();
@@ -60,7 +60,13 @@ public class MotorSpeedControl extends OpMode {
             resetProgram();
         }
 
-        if (powerMode == true) {
+        if (gamepad2.b) {
+            normalizeEncodersManually();
+        }
+
+        normalizeEncoders();
+
+        if (powerMode) {
             if (gamepad2.right_bumper) {
                 motorSpeed += .01;
             }
@@ -69,7 +75,7 @@ public class MotorSpeedControl extends OpMode {
             }
         }
 
-        else if (powerMode == false) {
+        else {
             if (gamepad2.right_bumper) {
                 targetHighGoalRPMs += 10;
             }
@@ -80,27 +86,24 @@ public class MotorSpeedControl extends OpMode {
 
             if (RPMs < targetHighGoalRPMs) {
                 telemetry.addLine("ENCODERS SLOW; INCREASING MOTOR SPEED");
-                motorSpeed += 0.01;
+                motorSpeed += 0.0001;
             }
 
             if (RPMs > targetHighGoalRPMs) {
-                telemetry.addLine("ENDCODERS FAST; DECREASING MOTOR SPEED");
-                motorSpeed -= 0.01;
+                telemetry.addLine("ENCODERS FAST; DECREASING MOTOR SPEED");
+                motorSpeed -= 0.0001;
             }
-
-
-
         }
 
-        Range.clip(motorSpeed, 0, 1);
+        motorSpeed = Range.clip(motorSpeed, minMotorPower, maxMotorPower);
         myMotor.setPower(motorSpeed);
 
 
-        if (gamepad2.dpad_right == true) {
+        if (gamepad2.dpad_right) {
             powerMode = true;
         }
 
-        if (gamepad2.dpad_left = false) {
+        if (gamepad2.dpad_left) {
             powerMode = false;
         }
 
@@ -135,6 +138,7 @@ public class MotorSpeedControl extends OpMode {
 
         telemetry.addData("Current Motor Power: ", myMotor.getPower());
         telemetry.addData("Current Time (ms): ", TeleOpTime.seconds());
+//        telemetry.addData("Time since last reset", TeleOpTime.seconds());
         telemetry.addData("Current Encoders: ", myMotor.getCurrentPosition());
         telemetry.addData("Encoders per second: ", encodersPerSecond);
 //        telemetry.addData("Encoders per 1 minute: ", (encodersPerSecond * 60));
@@ -145,6 +149,19 @@ public class MotorSpeedControl extends OpMode {
         TeleOpTime.reset();
         setMotorRunModes(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         setMotorRunModes(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        encodersResetCount = 0;
+    }
+
+    public void normalizeEncodersManually () {
+        TeleOpTime.reset();
+        encodersResetCount = encodersResetCount + myMotor.getCurrentPosition();
+    }
+
+    public void normalizeEncoders () {
+        if (TeleOpTime.seconds() >= 3) {
+            TeleOpTime.reset();
+            encodersResetCount = encodersResetCount + myMotor.getCurrentPosition();
+        }
     }
 
     public void initTeleOp () {
