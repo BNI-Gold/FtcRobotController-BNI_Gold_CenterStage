0e3fb4c6ea320be44c6bc332a8269c3ad636f613
ThomasTheTank28 <connor.reddington@mtbethelchristian.org>
2023-09-18 16:00:43 -0400

    Pushing To Github

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Compitition/CenterStage/Robots/CompBot.java
@@ -0,0 +1,80 @@
+package org.firstinspires.ftc.teamcode.Compitition.CenterStage.Robots;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.teamcode.Compitition.CenterStage.Drivetrains.MecanumDrive;
+
+public class CompBot extends MecanumDrive {
+
+
+
+        public HardwareMap hwBot = null;
+
+        public ElapsedTime currentTime = new ElapsedTime();
+
+        public ElapsedTime timer = new ElapsedTime();
+
+
+        public BNO055IMU imu;
+        public Orientation angles;
+        public Acceleration gravity;
+        public final double SPEED = .3;
+        public final double TOLERANCE = .4;
+
+        public CompBot (){}
+
+        public void initRobot(HardwareMap hwMap) {
+            hwBot = hwMap;
+
+            frontLeftMotor = hwBot.dcMotor.get("front_left_motor");
+            frontRightMotor = hwBot.dcMotor.get("front_right_motor");
+            rearLeftMotor = hwBot.dcMotor.get("rear_left_motor");
+            rearRightMotor = hwBot.dcMotor.get("rear_right_motor");
+
+            frontLeftMotor.setDirection(DcMotor.Direction.FORWARD);
+            rearLeftMotor.setDirection(DcMotor.Direction.FORWARD);
+            frontRightMotor.setDirection(DcMotor.Direction.REVERSE);
+            rearRightMotor.setDirection(DcMotor.Direction.REVERSE);
+
+            //Initialize Motor Run Mode for Robot
+            setMotorRunModes(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            setMotorRunModes(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+            frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            rearRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            rearLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+            currentTime.reset();
+
+
+            BNO055IMU.Parameters parametersimu = new BNO055IMU.Parameters();
+            parametersimu.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+            parametersimu.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+            parametersimu.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+
+            parametersimu.loggingEnabled = true;
+            parametersimu.loggingTag = "IMU";
+            parametersimu.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+            imu = hwBot.get(BNO055IMU.class, "imu");
+            imu.initialize(parametersimu);
+
+
+        }
+
+        public void stopMotors(){
+            frontLeftMotor.setPower(0);
+            frontRightMotor.setPower(0);
+            rearLeftMotor.setPower(0);
+            rearRightMotor.setPower(0);
+        }
+}
