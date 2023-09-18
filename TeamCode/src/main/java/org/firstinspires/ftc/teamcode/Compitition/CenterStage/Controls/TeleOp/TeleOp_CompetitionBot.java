b6c30c97ebb56a618b5885a0c8a6a45c545424f9
ThomasTheTank28 <connor.reddington@mtbethelchristian.org>
2023-09-18 16:05:02 -0400

    Pushing To Github

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Compitition/CenterStage/Controls/TeleOp/TeleOp_CompetitionBot.java
@@ -0,0 +1,114 @@
+package org.firstinspires.ftc.teamcode.Compitition.CenterStage.Controls.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.Compitition.CenterStage.Robots.CompBot;
+
+public class TeleOp_CompetitionBot extends OpMode {
+
+    double leftStickYVal;
+    double leftStickXVal;
+    double rightStickXVal;
+    double rightStickYVal;
+
+    double frontLeftSpeed;
+    double frontRightSpeed;
+    double rearLeftSpeed;
+    double rearRightSpeed;
+
+    double powerThreshold = 0;
+    double speedMultiply = 1;
+
+
+
+    public CompBot CompetitionBot = new CompBot();
+
+    public void init () {
+        CompetitionBot.initRobot(hardwareMap);
+    }
+    public void init_loop() {  }
+
+
+    public void start() {
+
+    }
+
+    public void loop(){
+        speedControl();
+        drive();
+        telemetryOutput();
+    }
+
+    public void drive() {
+
+        leftStickYVal = gamepad1.left_stick_y;
+        leftStickYVal = Range.clip(leftStickYVal, -1, 1);
+        leftStickXVal = -gamepad1.left_stick_x;
+        leftStickXVal = Range.clip(leftStickXVal, -1, 1);
+        rightStickXVal = -gamepad1.right_stick_x;
+        rightStickXVal = Range.clip(rightStickXVal, -1, 1);
+
+        frontLeftSpeed = leftStickYVal + leftStickXVal + rightStickXVal;
+        frontLeftSpeed = Range.clip(frontLeftSpeed, -1, 1);
+
+        frontRightSpeed = leftStickYVal - leftStickXVal - rightStickXVal;
+        frontRightSpeed = Range.clip(frontRightSpeed, -1, 1);
+
+        rearLeftSpeed = leftStickYVal - leftStickXVal + rightStickXVal;
+        rearLeftSpeed = Range.clip(rearLeftSpeed, -1, 1);
+
+        rearRightSpeed = leftStickYVal + leftStickXVal - rightStickXVal;
+        rearRightSpeed = Range.clip(rearRightSpeed, -1, 1);
+
+        if (frontLeftSpeed <= powerThreshold && frontLeftSpeed >= -powerThreshold) {
+            frontLeftSpeed = 0;
+            CompetitionBot.frontLeftMotor.setPower(frontLeftSpeed);
+        } else {
+            CompetitionBot.frontLeftMotor.setPower(frontLeftSpeed * speedMultiply);
+        }
+
+        if (frontRightSpeed <= powerThreshold && frontRightSpeed >= -powerThreshold){
+            frontRightSpeed = 0;
+            CompetitionBot.frontRightMotor.setPower(frontRightSpeed);
+        } else {
+            CompetitionBot.frontRightMotor.setPower(frontRightSpeed * speedMultiply);
+        }
+
+        if (rearLeftSpeed <= powerThreshold && rearLeftSpeed >= -powerThreshold) {
+            rearLeftSpeed = 0;
+            CompetitionBot.rearLeftMotor.setPower(rearLeftSpeed);
+        } else {
+            CompetitionBot.rearLeftMotor.setPower(rearLeftSpeed * speedMultiply);
+        }
+
+        if (rearRightSpeed <= powerThreshold && rearRightSpeed >= -powerThreshold){
+            rearRightSpeed = 0;
+            CompetitionBot.rearRightMotor.setPower(rearRightSpeed);
+        } else {
+            CompetitionBot.rearRightMotor.setPower(rearRightSpeed * speedMultiply);
+        }
+    }
+    public void telemetryOutput() {
+
+        telemetry.addData("pwr", "FL mtr: " + frontLeftSpeed);
+        telemetry.addData("pwr", "FR mtr: " + frontRightSpeed);
+        telemetry.addData("pwr", "RL mtr: " + rearLeftSpeed);
+        telemetry.addData("pwr", "RR mtr: " + rearRightSpeed);
+        telemetry.update();
+    }
+
+    public void speedControl() {
+
+        if (gamepad1.dpad_up) {
+            speedMultiply = 0.5;
+        }
+
+        else if (gamepad1.dpad_down) {
+            speedMultiply = 1;
+        }
+    }
+
+}
+
+}
